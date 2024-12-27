a890c3364d5bf526ba213615ffae4f272cf1187a
eioforkansi <eioforkansi@gmail.com>
2024-12-27 08:47:25 +0100

    Installations

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.scss";
+
+export default function App() {
+  return (
+    <div className="container">
+      <h1>
+        HealthCare<span>Mart</span>
+      </h1>
+    </div>
+  );
+}
